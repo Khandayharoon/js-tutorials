36f475e4bb329ea32da3d0657206922353f0acf3
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-06-19 10:26:41 +0530

    Add multiplication table to console

--- beginner-challenges/challenge-1/README.md
+++ beginner-challenges/challenge-1/README.md
@@ -2,7 +2,7 @@
 
 We are using the branch haroon-khanday for this assignment and the root branch for these tutorials. Make sure, you are branching from the correct branch.
 
-Use git to create the branch haroon-beginner-1. The command for creating and switching to the new branch is 'git checkout -b <branch-name>'. 
+Use git to create the branch . The command for creating and switching to the new branch is 'git checkout -b <branch-name>'.
 
 In this challenge, you will create an array with values from 0 to 10. Log the output to the console, so you can check it up in the browser.
 
---
+++ beginner-challenges/challenge-2/index.js
@@ -0,0 +1,29 @@
+function handleUserInput() {
+    let num = document.getElementById('inputNumber').value;
+    num = parseInt(num);
+
+    if (num < 1 || isNaN(num)) {
+        alert("Please enter a valid non-negative number.");
+        return;
+    }
+
+    let tableDiv = document.getElementById('tableDiv');
+
+    // Clear previous results
+    tableDiv.innerHTML = '';
+
+    for (let i = 1; i <= 100; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.id = `row${i}`;
+        rowDiv.className = 'row';
+
+        const newP = document.createElement('p');
+        newP.id = `result${i}`;
+        newP.className = 'result';
+        console.log(`${num} x ${i} = ${num * i}`);
+        newP.textContent = `${num} x ${i} = ${num * i}`;
+
+        rowDiv.appendChild(newP);
+        tableDiv.appendChild(rowDiv);
+    }
+}
