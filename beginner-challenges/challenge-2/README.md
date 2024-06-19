30b6653348673cf2d33d30f224fe8388e6b5acd8
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-06-19 17:04:55 +0530

    Complete the Challenge 2

--- beginner-challenges/challenge-2/README.md
+++ beginner-challenges/challenge-2/README.md
@@ -2,7 +2,7 @@
 
 We are using the branch haroon-khanday for this assignment and the root branch for these tutorials. Make sure, you are branching from the correct branch.
 
-Use git to create the branch haroon-beginner-2. The command for creating and switching to the new branch is 'git checkout -b <branch-name>'. 
+Use git to create the branch haroon-beginner-2. The command for creating and switching to the new branch is 'git checkout -b <branch-name>'.
 
 In this challenge, you will create a table with the results of all multiplication of 1 to 10 (The first element in the table is 1 and the last 100). Log the output to the console, so you can check it up in the browser.
 
--- beginner-challenges/challenge-2/index.js
+++ beginner-challenges/challenge-2/index.js
@@ -8,24 +8,24 @@ function handleUserInput() {
         return;
     }
 
-    // let tableDiv = document.getElementById('tableDiv');
+    let tableDiv = document.getElementById('tableDiv');
 
     // Clear previous results
-    // tableDiv.innerHTML = '';
+    tableDiv.innerHTML = '';
 
-    for (let i = 1; i <= 200; i++) {
-        // const rowDiv = document.createElement('div');
-        // rowDiv.id = `row${i}`;
-        // rowDiv.className = 'row';
+    for (let i = 1; i <=100; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.id = `row${i}`;
+        rowDiv.className = 'row';
 
-        // const newP = document.createElement('p');
-        // newP.id = `result${i}`;
-        // newP.className = 'result';
+        const newP = document.createElement('p');
+        newP.id = `result${i}`;
+        newP.className = 'result';
         console.log(`${num} x ${i} = ${num * i}`);
-        // newP.textContent = `${num} x ${i} = ${num * i}`;
+        newP.textContent = `${num} x ${i} = ${num * i}`;
 
-        // rowDiv.appendChild(newP);
-        // tableDiv.appendChild(rowDiv);
+        rowDiv.appendChild(newP);
+        tableDiv.appendChild(rowDiv);
     }
 }
 
