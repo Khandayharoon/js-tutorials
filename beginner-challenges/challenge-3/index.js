12c0f4bb04a625a7bc0279fa10ccf07668b317ab
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-06-20 12:01:02 +0530

    Add the basic functionalties to the basic converter

--- beginner-challenges/challenge-3/index.js
+++ beginner-challenges/challenge-3/index.js
@@ -1,6 +1,31 @@
-let num = prompt("Enter the number of kilometers you want to convert:");
-let miles = num * 0.62137119;
-miles = miles.toFixed(2);
-// Round to 2 decimal places
-alert(`${num} kilometers is ${miles} miles`);
-console.log(`${num} kilometers is ${miles} miles`);
+function handleInput(){
+    let inputUnit = document.getElementById('inputUnit').value;
+    let convertUnit = document.getElementById('units').value;
+
+if(convertUnit === "Miles"){
+    let miles = inputUnit * 0.62137119;
+    miles = miles.toFixed(2);
+    alert(`${inputUnit } kilometers is ${miles} miles`);
+    console.log(`${inputUnit } kilometers is ${miles} miles`);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = prompt("Enter the number of kilometers you want to convert:");
+// let miles = num * 0.62137119;
+// miles = miles.toFixed(2);
+// // Round to 2 decimal places
+// alert(`${num} kilometers is ${miles} miles`);
+// console.log(`${num} kilometers is ${miles} miles`);
