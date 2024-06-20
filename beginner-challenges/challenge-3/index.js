1902e38e9be7196320463aeddc762bd138b2cd80
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-06-20 11:35:07 +0530

    Basic Unit Converter

---
+++ beginner-challenges/challenge-3/index.js
@@ -0,0 +1,6 @@
+let num = prompt("Enter the number of kilometers you want to convert:");
+let miles = num * 0.62137119;
+miles = miles.toFixed(2);
+// Round to 2 decimal places
+alert(`${num} kilometers is ${miles} miles`);
+console.log(`${num} kilometers is ${miles} miles`);
