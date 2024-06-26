e0d880d1f53718507b25b490c085397694039576
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-06-26 11:40:35 +0530

    as per the changes you wanted me to make i did

--- beginner-challenges/challenge-1/index.js
+++ beginner-challenges/challenge-1/index.js
@@ -1,30 +1,38 @@
-
-//below function will gerrate the array with the userinput
-function generateArray(num){
-    let Array =[];
-    for(let i=1; i<=num; i++){
-        Array.push(i);
+/**
+ * Generates an array of numbers from 1 to the specified number.
+ *
+ * @param {number} num - The upper limit of the range (inclusive).
+ * @returns {number[]} An array containing numbers from 1 to the specified number.
+ */
+function generateArray(num) {
+    const generatedArray = [];
+    for (let i = 1; i <= num; i++) {
+        generatedArray.push(i);
     }
-
-    return Array;
+    return generatedArray;
 }
 
+/**
+ * Handles the user input and shows generatedArray on the HTML document (DOM).
+ */
+function handleUserInput(event) {
+    event.preventDefault(); // Prevent the form from submitting and refreshing the page
 
-//handle the user input and show Array on HTML Document (DOM)
-function handleUserInput(){
     let num = document.getElementById('numberInput').value;
     if (num.toLowerCase() === "quit") {
-        document.getElementById('result').innerText = "You have quit the input.";
+        document.getElementById("result").innerText = "You have quit the input";
         return;
     }
 
     num = parseInt(num);
-    if(isNaN(num) || num < 0){
-        document.getElementById('result').innerText = "Please enter a valid or Non-Negative number.";
+    if (isNaN(num) || num < 0) {
+        document.getElementById("result").innerText = "Please enter a valid or non-negative number";
         return;
     }
 
-    let resultArray  = generateArray(num);
-    document.getElementById('result').innerText = `Generated Array is : [${resultArray}]`;
+    let resultArray = generateArray(num);
+    document.getElementById("result").innerText = `Generated Array is: [${resultArray}]`;
     console.log(resultArray);
 }
+
+document.getElementById('numberForm').addEventListener('submit', handleUserInput);
