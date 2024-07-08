b568037b21753c770d9380e609d9aa0fef8e703c
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-07-08 09:52:05 +0530

    all done

--- beginner-challenges/challenge-4/index.js
+++ beginner-challenges/challenge-4/index.js
@@ -17,3 +17,44 @@ function handleUserInput(){
     });
     
 }
+
+
+function handleUserInputTable() {
+    document.getElementById("TableForm").addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        let num = document.getElementById("table_form").value;
+        num = parseInt(num);
+
+        if (isNaN(num) || num <= 0) {
+            alert("Please enter a valid non-negative number.");
+            return;
+        }
+
+        let table_div = document.getElementById("table-div");
+        
+        // Clear previous table if it exists
+        table_div.innerHTML = '';
+
+        let table = document.createElement("table");
+        table.id = "2dArray";
+        let sum = 0;
+
+        for (let i = 0; i < num; i++) {
+            let row = document.createElement("tr");
+            for (let j = 0; j < num; j++) {
+                let cell = document.createElement("td");
+                // cell.id = "cell";
+                let value = Math.floor(Math.random() * 100); // Generate random number between 0 and 99
+                cell.textContent = value;
+                sum += value;
+                row.appendChild(cell);
+            }
+            table.appendChild(row);
+        }
+        table_div.appendChild(table);
+
+        let table_sum = document.getElementById("table_sum");
+        table_sum.innerText = sum;
+    });
+}
