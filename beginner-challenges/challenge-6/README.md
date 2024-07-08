1c90820f25acab0072f45e973d89af66d3741175
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-07-08 11:30:51 +0530

    all done with sort an array

--- beginner-challenges/challenge-6/README.md
+++ beginner-challenges/challenge-6/README.md
@@ -1,7 +1,8 @@
 # Challenge 6: Sort an array
+
 We are using the branch haroon-khanday for this assignment and the root branch for these tutorials. Make sure, you are branching from the correct branch.
 
-Use git to create the branch haroon-beginner-6. The command for creating and switching to the new branch is 'git checkout -b <branch-name>'. 
+Use git to create the branch haroon-beginner-6. The command for creating and switching to the new branch is 'git checkout -b <branch-name>'.
 
 This challenge is about sorting an array. Create an array with numbers and sort it from lowest to highest. Print the result to the console when you are done.
 
--- beginner-challenges/challenge-6/index.js
+++ beginner-challenges/challenge-6/index.js
@@ -1,23 +1,69 @@
-let givenArray = [2, 4, 6, 7, 8, 1, 9, 0, 12, 67];
+// let givenArray = [2, 4, 6, 7, 8, 1, 9, 0, 12, 67];
 
-/**
- * takes givenArray as input and sorts with Bubblesort.
- *
- * @param {Array} givenArray - which is  unsorted Array.
- * @returns {Array[]} An array which is sorted in Assending order.
- */
-function sortArray(givenArray) {
+// /**
+//  * takes givenArray as input and sorts with Bubblesort.
+//  *
+//  * @param {Array} givenArray - which is  unsorted Array.
+//  * @returns {Array[]} An array which is sorted in Assending order .
+//  */
+// function sortArray(givenArray) {
+//     let n = givenArray.length;
+//     for (let i = 0; i < n - 1; i++) {
+//         for (let j = 0; j < n - 1 - i; j++) {
+//             if (givenArray[j] > givenArray[j + 1]) {
+//                 let temp = givenArray[j];
+//                 givenArray[j] = givenArray[j + 1];
+//                 givenArray[j + 1] = temp;
+//             }
+//         }
+//     }
+//     console.log(givenArray);
+// }
+
+// sortArray(givenArray);
+
+function sortArrayAscendingOrder() {
+
+        let givenArrayString = document.getElementById("inputArray").value.trim();
+     
+        givenArrayString = givenArrayString.replace(/[\[\]]/g, ''); // Remove square brackets
+        let givenArray = givenArrayString.split(',') // Split by comma
+                                         .map(item => parseFloat(item.trim())); // Convert to numbers
+
+        let n = givenArray.length;
+        for (let i = 0; i < n - 1; i++) {
+            for (let j = 0; j < n - 1 - i; j++) {
+                if (givenArray[j] > givenArray[j + 1]) {
+                    let temp = givenArray[j];
+                    givenArray[j] = givenArray[j + 1];
+                    givenArray[j + 1] = temp;
+                }
+            }
+        }
+
+        document.getElementById("sortArrayAscendingOrder").innerText = "[" + givenArray.join(', ') + "]"; // Join sorted array for display
+}
+
+
+
+
+function sortArrayDescendingOrder(){
+    let givenArrayString = document.getElementById("inputArray").value.trim();
+     
+    givenArrayString = givenArrayString.replace(/[\[\]]/g, ''); // Remove square brackets
+    let givenArray = givenArrayString.split(',') // Split by comma
+                                     .map(item => parseFloat(item.trim())); // Convert to numbers
+
     let n = givenArray.length;
     for (let i = 0; i < n - 1; i++) {
         for (let j = 0; j < n - 1 - i; j++) {
-            if (givenArray[j] > givenArray[j + 1]) {
+            if (givenArray[j] < givenArray[j + 1]) {
                 let temp = givenArray[j];
                 givenArray[j] = givenArray[j + 1];
                 givenArray[j + 1] = temp;
             }
         }
     }
-    console.log(givenArray);
-}
 
-sortArray(givenArray);
+    document.getElementById("sortArrayDescendingOrder").innerText = "[" + givenArray.join(', ') + "]"; // Join sorted array for display
+}
