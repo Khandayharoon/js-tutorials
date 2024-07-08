11f15e0ba6cd87908479e953da556b9c09f2b7e1
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-07-08 10:30:26 +0530

    all done with Array

--- beginner-challenges/challenge-5/index.js
+++ beginner-challenges/challenge-5/index.js
@@ -1,11 +1,25 @@
-let givenArray = [2, 4, 5, 6, 7, 8, 9, 0, 9];
+function reverseTheArray() {
+    document.getElementById("inputForm").addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+      
+        let givenArrayString = document.getElementById("inputArray").value.trim();
+        
+     
+        givenArrayString = givenArrayString.replace(/[\[\]]/g, '');
 
-function reverseTheArray(givenArray) {
-    let reversedArray = [];
-    for (let i = givenArray.length - 1; i >= 0; i--) {
-        reversedArray.push(givenArray[i]);
-    }
-    console.log(reversedArray);
+        let givenArray = givenArrayString.split(',').map(item => item.trim());
+        
+     
+        let reversedArray = [];
+        for (let i = givenArray.length - 1; i >= 0; i--) {
+            reversedArray.push(givenArray[i]);
+        }
+
+        document.getElementById("output").innerText = "[" + reversedArray + "]";
+        console.log("[" + reversedArray + "]");
+    });
 }
 
-reverseTheArray(givenArray);
+// Call the function to set up the event listener
+reverseTheArray();
