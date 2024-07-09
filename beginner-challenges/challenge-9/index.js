b3f3b4ad70880f48a307a900ddf2c1dba843b8ad
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-07-09 15:10:45 +0530

    done first part

---
+++ beginner-challenges/challenge-9/index.js
@@ -0,0 +1,11 @@
+let num = 54;
+function checkNumberDividable(num){
+    if(num % 10 === 0){
+        console.log(`${num} is  dividable by Ten`)
+    }
+    else
+    {
+        console.log(`${num} is not  dividable by Ten`)
+    }
+}
+checkNumberDividable(num);
